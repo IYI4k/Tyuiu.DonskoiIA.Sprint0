c39bcc5a3cd167184159fe8be849bab5abb1ee85
IYI4k <IYI4k@yandex.ru>
2024-09-11 18:14:17 +0500

    task1

---
+++ Tyuiu.DonskoiIA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.DonskoiIA.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Донской Иван";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
